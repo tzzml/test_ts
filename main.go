7ea52725e4c0b93df88033e50f894184fbb44084
zhuminglei <zhuminglei@zork.com.cn>
2025-09-02 13:41:51 +0800

    add "add" fun

--- main.go
+++ main.go
@@ -2,7 +2,15 @@ package main
 
 import "fmt"
 
+func add(a, b int) int {
+	return a + b
+}
+
 func main() {
 	fmt.Println("Hello, World!")
 	fmt.Println("This is a sample Go program.")
+	
+	// 示例：使用add函数
+	result := add(5, 3)
+	fmt.Printf("5 + 3 = %d\n", result)
 }
